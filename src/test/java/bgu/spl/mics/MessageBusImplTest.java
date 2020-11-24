ebcd415b168c0b66c9d5772c85a25af7509cf7b5
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-11-24 23:42:39 +0200

    Added empty tests to MessageBusImplTest

--- src/test/java/bgu/spl/mics/MessageBusImplTest.java
+++ src/test/java/bgu/spl/mics/MessageBusImplTest.java
@@ -7,9 +7,41 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MessageBusImplTest {
-    private MessageBusImpl myMessageBus;
+    private MessageBusImpl testMessage;
     @BeforeEach
     void setUp() {
-        myMessageBus = new MessageBusImpl();
+        testMessage = new MessageBusImpl();
+    }
+
+    @Test
+    void subscribeEvent() {
+    }
+
+    @Test
+    void subscribeBroadcast() {
+    }
+
+    @Test
+    void complete() {
+    }
+
+    @Test
+    void sendBroadcast() {
+    }
+
+    @Test
+    void sendEvent() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void unregister() {
+    }
+
+    @Test
+    void awaitMessage() {
     }
 }
