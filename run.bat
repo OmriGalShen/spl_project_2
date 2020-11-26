a819951575e1437a6afe3d583a565a7e77a10813
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-11-26 12:45:13 +0200

    Small fixes to run files

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-mvn exec:java -Dexec.mainClass="bgu.spl.mics.application.Main" -Dexec.args="example/input.json example/output.json"
+mvn compile && mvn exec:java -Dexec.mainClass="bgu.spl.mics.application.Main" -Dexec.args="example/input.json example/output.json"
