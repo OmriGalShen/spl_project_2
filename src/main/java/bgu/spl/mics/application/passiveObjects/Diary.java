a42642dbe75af5fe2851c42432e0fc9f1f214540
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-11-29 20:23:14 +0200

    Made Diary singleton

--- src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
@@ -9,10 +9,18 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Do not add to this class nothing but a single constructor, getters and setters.
  */
 public class Diary {
+    private static Diary instance;
     private int totalAttacks;
     private long HanSoloFinish, C3POFinish, R2D2Deactivate, LeiaTerminate,
             HanSoloTerminate,C3POTerminate,R2D2Terminate, LandoTerminate;
-    public Diary(){}
+
+    private Diary(){}
+
+    public static Diary getInstance() {
+        if(instance==null)
+            return new Diary();
+        return instance;
+    }
 
     public int getTotalAttacks() {
         return totalAttacks;
