d5649ee7bec621da2909d7f6d6944a33b5c53ce0
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-11-29 16:40:02 +0200

    Starting implementation of Diary

--- src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
@@ -9,5 +9,80 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Do not add to this class nothing but a single constructor, getters and setters.
  */
 public class Diary {
+    private int totalAttacks;
+    private long HanSoloFinish, C3POFinish, R2D2Deactivate, LeiaTerminate,
+            HanSoloTerminate,C3POTerminate,R2D2Terminate, LandoTerminate;
+    public Diary(){}
 
+    public int getTotalAttacks() {
+        return totalAttacks;
+    }
+
+    public void setTotalAttacks(int totalAttacks) {
+        this.totalAttacks = totalAttacks;
+    }
+
+    public long getHanSoloFinish() {
+        return HanSoloFinish;
+    }
+
+    public void setHanSoloFinish(long hanSoloFinish) {
+        HanSoloFinish = hanSoloFinish;
+    }
+
+    public long getC3POFinish() {
+        return C3POFinish;
+    }
+
+    public void setC3POFinish(long c3POFinish) {
+        C3POFinish = c3POFinish;
+    }
+
+    public long getR2D2Deactivate() {
+        return R2D2Deactivate;
+    }
+
+    public void setR2D2Deactivate(long r2D2Deactivate) {
+        R2D2Deactivate = r2D2Deactivate;
+    }
+
+    public long getLeiaTerminate() {
+        return LeiaTerminate;
+    }
+
+    public void setLeiaTerminate(long leiaTerminate) {
+        LeiaTerminate = leiaTerminate;
+    }
+
+    public long getHanSoloTerminate() {
+        return HanSoloTerminate;
+    }
+
+    public void setHanSoloTerminate(long hanSoloTerminate) {
+        HanSoloTerminate = hanSoloTerminate;
+    }
+
+    public long getC3POTerminate() {
+        return C3POTerminate;
+    }
+
+    public void setC3POTerminate(long c3POTerminate) {
+        C3POTerminate = c3POTerminate;
+    }
+
+    public long getR2D2Terminate() {
+        return R2D2Terminate;
+    }
+
+    public void setR2D2Terminate(long r2D2Terminate) {
+        R2D2Terminate = r2D2Terminate;
+    }
+
+    public long getLandoTerminate() {
+        return LandoTerminate;
+    }
+
+    public void setLandoTerminate(long landoTerminate) {
+        LandoTerminate = landoTerminate;
+    }
 }
