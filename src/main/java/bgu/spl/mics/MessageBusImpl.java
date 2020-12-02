cbe8f755132fe5e37a50aea566b7b82594bd2de0
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-12-02 23:47:16 +0200

    Copied javadoc from MessageBus to MessageBusImpl

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -40,34 +40,77 @@ public class MessageBusImpl implements MessageBus {
 		}
 		return instance;
 	}
-	
+
+	/**
+	 * Subscribes {@code m} to receive {@link Event}s of type {@code type}.
+	 * <p>
+	 * @param <T>  The type of the result expected by the completed event.
+	 * @param type The type to subscribe to,
+	 * @param m    The subscribing micro-service.
+	 */
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
 		
 	}
 
+	/**
+	 * Subscribes {@code m} to receive {@link Broadcast}s of type {@code type}.
+	 * <p>
+	 * @param type 	The type to subscribe to.
+	 * @param m    	The subscribing micro-service.
+	 */
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
 		
     }
 
+	/**
+	 * Notifies the MessageBus that the event {@code e} is completed and its
+	 * result was {@code result}.
+	 * When this method is called, the message-bus will resolve the {@link Future}
+	 * object associated with {@link Event} {@code e}.
+	 * <p>
+	 * @param <T>    The type of the result expected by the completed event.
+	 * @param e      The completed event.
+	 * @param result The resolved result of the completed event.
+	 */
 	@Override @SuppressWarnings("unchecked")
 	public <T> void complete(Event<T> e, T result) {
 		
 	}
 
+	/**
+	 * Adds the {@link Broadcast} {@code b} to the message queues of all the
+	 * micro-services subscribed to {@code b.getClass()}.
+	 * <p>
+	 * @param b 	The message to added to the queues.
+	 */
 	@Override
 	public void sendBroadcast(Broadcast b) {
 		
 	}
 
-	
+	/**
+	 * Adds the {@link Event} {@code e} to the message queue of one of the
+	 * micro-services subscribed to {@code e.getClass()} in a round-robin
+	 * fashion. This method should be non-blocking.
+	 * <p>
+	 * @param <T>    	The type of the result expected by the event and its corresponding future object.
+	 * @param e     	The event to add to the queue.
+	 * @return {@link Future<T>} object to be resolved once the processing is complete,
+	 * 	       null in case no micro-service has subscribed to {@code e.getClass()}.
+	 */
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
 		
         return null;
 	}
 
+	/**
+	 * Allocates a message-queue for the {@link MicroService} {@code m}.
+	 * <p>
+	 * @param m the micro-service to create a queue for.
+	 */
 	@Override
 	public void register(MicroService m) {
 		LinkedList<Message> messagesQueue = new LinkedList<Message>();
@@ -76,12 +119,35 @@ public class MessageBusImpl implements MessageBus {
 		microServiceSubscriptions.put(m,messageTypeQueue);
 	}
 
+	/**
+	 * Removes the message queue allocated to {@code m} via the call to
+	 * {@link #register(bgu.spl.mics.MicroService)} and cleans all references
+	 * related to {@code m} in this message-bus. If {@code m} was not
+	 * registered, nothing should happen.
+	 * <p>
+	 * @param m the micro-service to unregister.
+	 */
 	@Override
 	public void unregister(MicroService m) {
 		microServiceMessages.remove(m);
 		microServiceSubscriptions.remove(m);
 	}
 
+	/**
+	 * Using this method, a <b>registered</b> micro-service can take message
+	 * from its allocated queue.
+	 * This method is blocking meaning that if no messages
+	 * are available in the micro-service queue it
+	 * should wait until a message becomes available.
+	 * The method should throw the {@link IllegalStateException} in the case
+	 * where {@code m} was never registered.
+	 * <p>
+	 * @param m The micro-service requesting to take a message from its message
+	 *          queue.
+	 * @return The next message in the {@code m}'s queue (blocking).
+	 * @throws InterruptedException if interrupted while waiting for a message
+	 *                              to became available.
+	 */
 	@Override
 	public Message awaitMessage(MicroService m) throws InterruptedException {
 		
