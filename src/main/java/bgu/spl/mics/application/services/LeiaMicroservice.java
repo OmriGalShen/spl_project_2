1d38dccdd6001317e6357443a83860be89f175ef
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-12-06 17:36:15 +0200

    Improved Leia implementation

--- src/main/java/bgu/spl/mics/application/services/LeiaMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/LeiaMicroservice.java
@@ -11,32 +11,28 @@ import bgu.spl.mics.application.passiveObjects.Attack;
 import bgu.spl.mics.application.passiveObjects.Diary;
 
 /**
- * LeiaMicroservices Initialized with Attack objects, and sends them as  {@link AttackEvents}.
+ * LeiaMicroservices Initialized with Attack objects, and sends them as  {@link AttackEvent}.
  * This class may not hold references for objects which it is not responsible for:
- * {@link AttackEvents}.
+ * {@link AttackEvent}.
  *
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class LeiaMicroservice extends MicroService {
-	private Attack[] attacks;
 	private Future[] futures;
 	private AttackEvent[] attackEvents;
 	
     public LeiaMicroservice(Attack[] attacks) {
         super("Leia");
-		this.attacks = attacks;
         this.futures = new Future[attacks.length];
-        this.attackEvents = new AttackEvent[attacks.length];
-
+        for (int i = 0; i <attacks.length ; i++) {
+            attackEvents[i] = new AttackEvent(attacks[i]);
+        }
     }
 
     @Override
     protected void initialize() {
         MessageBusImpl messageBus = MessageBusImpl.getInstance();
-        for (int i = 0; i <attacks.length ; i++) {
-            attackEvents[i] = new AttackEvent(attacks[i]);
-        }
         for (int i = 0; i < attackEvents.length; i++) {
             futures[i] = messageBus.sendEvent(attackEvents[i]);
         }
