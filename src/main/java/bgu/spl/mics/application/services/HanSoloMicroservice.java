7014287a93e0d1eddbaf1bacf9bd052d8ed078c7
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-12-06 17:31:21 +0200

    Add attack event response for Hans

--- src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
@@ -1,6 +1,7 @@
 package bgu.spl.mics.application.services;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import bgu.spl.mics.Callback;
 import bgu.spl.mics.MessageBusImpl;
@@ -12,17 +13,18 @@ import bgu.spl.mics.application.passiveObjects.Diary;
 import bgu.spl.mics.application.passiveObjects.Ewoks;
 
 /**
- * HanSoloMicroservices is in charge of the handling {@link AttackEvents}.
+ * HanSoloMicroservices is in charge of the handling {@link AttackEvent}.
  * This class may not hold references for objects which it is not responsible for:
- * {@link AttackEvents}.
+ * {@link AttackEvent}.
  *
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class HanSoloMicroservice extends MicroService {
-
-    public HanSoloMicroservice() {
+    private final Ewoks ewoks;
+    public HanSoloMicroservice(){
         super("Han");
+        this.ewoks = Ewoks.getInstance();
     }
 
 
@@ -32,7 +34,24 @@ public class HanSoloMicroservice extends MicroService {
         this.subscribeEvent(AttackEvent.class, new Callback<AttackEvent>() {
             @Override
             public void call(AttackEvent c) {
-                // TODO: add callback
+                List<Integer> serials = c.getAttack().getSerials();
+                for(int serial: serials ){
+                    ewoks.acquire(serial);
+                    ewoks.release(serial);
+                }
+                try {
+                    Thread.sleep(c.getAttack().getDuration());
+                    Diary.getInstance().setHanSoloFinish(System.currentTimeMillis());
+                    MessageBusImpl.getInstance().complete(c,true);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        this.subscribeBroadcast(TerminateBroadcast.class, new Callback<TerminateBroadcast>() {
+            @Override
+            public void call(TerminateBroadcast c) {
+                // TODO : call this object .terminate()
             }
         });
     }
