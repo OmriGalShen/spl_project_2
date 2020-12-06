c2c61cd94966956328a3f3661c578d57c946fa31
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-12-06 17:28:14 +0200

    Initial implementation of Ewoks

--- src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
@@ -1,8 +1,6 @@
 package bgu.spl.mics.application.passiveObjects;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 
 
 /**
@@ -14,5 +12,46 @@ import java.util.List;
  * You can add ONLY private methods and fields to this class.
  */
 public class Ewoks {
+    private static Ewoks instance;
+    private List<Ewok> ewoksList;
 
+    private Ewoks(){
+    }
+    public static Ewoks getInstance(){ //Singleton pattern
+        if(instance == null){
+            //only on creation of first instance synchronize:
+            // this is to make sure only one thread creates the first instance
+            synchronized (Ewoks.class){
+                if(instance==null)
+                    instance = new Ewoks();
+            }
+        }
+        return instance;
+    }
+    public void initialize(int size){
+        this.ewoksList = Collections.synchronizedList(new ArrayList<>(size));
+    }
+    public void acquire(int serialNumber){
+        Ewok e = ewoksList.get(serialNumber);
+        while (!e.available){
+            synchronized (this){
+                try {
+                    wait();
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+            }
+
+        }
+        ewoksList.get(serialNumber).acquire();
+    }
+    public void release(int serialNumber){
+        ewoksList.get(serialNumber).release();
+        synchronized (this) {
+            notifyAll();
+        }
+    }
+    public boolean isAvailable(int serialNumber){
+        return ewoksList.get(serialNumber).available;
+    }
 }
