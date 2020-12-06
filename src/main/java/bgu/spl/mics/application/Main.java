9632e70d8038ebef16c977b4fab5b4c6ee1321b4
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-12-06 02:36:12 +0200

    Major fix - added json input and output!

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -22,63 +22,43 @@ public class Main {
 	public static void main(String[] args) {
 		if(args.length!=2)
 		{
-			System.out.println("Valid input arguments weren't given");
+			System.out.println("No valid input arguments were given");
 			System.out.println("Please provide valid input.json path and output.json path");
 			return; //exit main
 		}
 		String inputFilePath = args[0]; //input file path from arguments
 		String outputFilePath = args[1];//output file path from arguments
+		Input input;
+		try{
+			input= getInputFromJson(inputFilePath);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 
-		handleJsonInput(inputFilePath);
+
+
+		Diary recordDiary = Diary.getInstance();
+		try {
+			diaryToJson(outputFilePath,recordDiary);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
-
-	private static void handleJsonInput(String inputFilePath) {
+	public static Input getInputFromJson(String filePath) throws IOException {
 		Gson gson = new Gson();
-		String jsonString = jsonToString(inputFilePath);
-		if(jsonString.isEmpty()) {
-			System.out.println("Empty json or error reading input json file");
-			return; //exit main
-		}
-		try {
-			JsonObject json = gson.fromJson(jsonString, JsonObject.class);
-			long R2D2 = Long.parseLong(json.get("R2D2").toString());
-			long Lando = Long.parseLong(json.get("Lando").toString());
-			long Ewoks = Long.parseLong(json.get("Ewoks").toString());
-			JsonArray attacksJson = json.get("attacks").getAsJsonArray();
-			List<Attack> attackList = new ArrayList<>();
-			Type listType = new TypeToken<List<Integer>>() {
-			}.getType();
-			for (int i = 0; i < attacksJson.size(); i++) {
-				int duration = Integer.parseInt(attacksJson.get(i).getAsJsonObject().get("duration").toString());
-				String serialsString = attacksJson.get(i).getAsJsonObject().get("serials").toString();
-				List<Integer> serials = gson.fromJson(serialsString, listType);
-				attackList.add(new Attack(serials, duration));
-			}
-		}
-		catch (Exception e) {
-			System.out.println("Problem parsing input json to objects");
-			e.printStackTrace();
+		try (Reader reader = new FileReader(filePath)) {
+			return gson.fromJson(reader, Input.class);
 		}
 	}
-	private static String jsonToString(String inputFilePath)
-	{
-		File inputFile;
 
-		String jsonString= "";
-		try {
-			inputFile = new File(inputFilePath);
-			Scanner myReader = new Scanner(inputFile);
-
-			while (myReader.hasNextLine()) {
-				String data = myReader.nextLine();
-				jsonString+=data;
-			}
-			myReader.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("Input json path is not valid");
-			e.printStackTrace();
+	public static void diaryToJson(String filePath, Diary recordDiary) throws IOException {
+		Gson gson = new Gson();
+		try (Writer writer = new FileWriter(filePath)) {
+			gson.toJson(recordDiary, writer);
 		}
-		return jsonString;
 	}
 }
+
+
