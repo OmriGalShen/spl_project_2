d1e441ecea76b03bfa5417a6fb0e249093a2d089
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-12-06 02:55:48 +0200

    Comments and style

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -20,29 +20,36 @@ import com.google.gson.stream.JsonReader;
  */
 public class Main {
 	public static void main(String[] args) {
-		if(args.length!=2)
-		{
+		// <----------input ---------->
+		if(args.length!=2) {
 			System.out.println("No valid input arguments were given");
 			System.out.println("Please provide valid input.json path and output.json path");
 			return; //exit main
 		}
 		String inputFilePath = args[0]; //input file path from arguments
 		String outputFilePath = args[1];//output file path from arguments
-		Input input;
+		Input input=null;
 		try{
 			input= getInputFromJson(inputFilePath);
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 		}
+		// <----------input ---------->
 
+		// <--------main program ---------->
+//		if(input!=null)
+//			starWars(input);
+		// <--------main program ---------->
 
+		// <----------output ---------->
 		Diary recordDiary = Diary.getInstance();
 		try {
 			diaryToJson(outputFilePath,recordDiary);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		// <----------output ---------->
 
 	}
 	private static Input getInputFromJson(String filePath) throws IOException {
@@ -57,6 +64,28 @@ public class Main {
 		try (Writer writer = new FileWriter(filePath)) {
 			gson.toJson(recordDiary, writer);
 		}
+	}
+
+	private static void starWars(Input input)
+	{
+		Thread leiaThread = new Thread(new LeiaMicroservice(input.getAttacks()));
+		Thread c3p0Thread = new Thread(new C3POMicroservice());
+		Thread hansThread = new Thread(new HanSoloMicroservice());
+		Thread landoThread = new Thread(new LandoMicroservice(input.getLando()));
+		Thread r2d2Thread = new Thread(new R2D2Microservice(input.getR2D2()));
+		leiaThread.start();
+		c3p0Thread.start();
+		hansThread.start();
+		landoThread.start();
+		r2d2Thread.start();
+		try{
+			leiaThread.join();
+			c3p0Thread.join();
+			hansThread.join();
+			landoThread.join();
+			r2d2Thread.join();
+		}
+		catch (InterruptedException e){}
 	}
 }
 
