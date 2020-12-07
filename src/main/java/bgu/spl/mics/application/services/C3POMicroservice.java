78c5567b42d8e0140804ba2f975b36f36bd94cfa
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-12-07 10:18:08 +0200

    Major implementation of C3POMicroservice

--- src/main/java/bgu/spl/mics/application/services/C3POMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/C3POMicroservice.java
@@ -1,12 +1,10 @@
 package bgu.spl.mics.application.services;
 import java.util.List;
 
-import bgu.spl.mics.Callback;
 import bgu.spl.mics.MessageBusImpl;
 import bgu.spl.mics.MicroService;
 import bgu.spl.mics.application.messages.AttackEvent;
 import bgu.spl.mics.application.messages.TerminateBroadcast;
-import bgu.spl.mics.application.passiveObjects.Attack;
 import bgu.spl.mics.application.passiveObjects.Diary;
 import bgu.spl.mics.application.passiveObjects.Ewoks;
 
@@ -29,23 +27,25 @@ public class C3POMicroservice extends MicroService {
 
     @Override
     protected void initialize() {
-        MessageBusImpl messageBus = MessageBusImpl.getInstance();
-        this.subscribeEvent(AttackEvent.class, new Callback<AttackEvent>() {
-            @Override
-            public void call(AttackEvent c) {
-                List<Integer> serials = c.getAttack().getSerials();
-                for(int serial: serials ){
-                    ewoks.acquire(serial);
-                    ewoks.release(serial);
-                }
-                try {
-                    Thread.sleep(c.getAttack().getDuration());
-                    Diary.getInstance().setHanSoloFinish(System.currentTimeMillis());
-                    MessageBusImpl.getInstance().complete(c,true);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        this.subscribeEvent(AttackEvent.class, c -> {
+            List<Integer> serials = c.getAttack().getSerials();
+            for(int serial: serials ){
+                ewoks.acquire(serial);
+                ewoks.release(serial);
+            }
+            try {
+                Thread.sleep(c.getAttack().getDuration());
+                Diary.getInstance().setHanSoloFinish(System.currentTimeMillis());
+                MessageBusImpl.getInstance().complete(c,true);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         });
+        // -- subscribe to TerminateBroadcast and terminate accordingly --
+        this.subscribeBroadcast(TerminateBroadcast.class, c -> {
+            Diary.getInstance().setC3POTerminate(System.currentTimeMillis());
+            this.terminate();
+        });
+        //------------------------------------------------------------------
     }
 }
