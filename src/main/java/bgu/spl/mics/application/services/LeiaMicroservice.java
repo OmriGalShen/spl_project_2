e5e8d9871ae7d05aedba5b48cbb7e88d10477c19
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-12-07 10:19:09 +0200

    Fixed terminate conditions

--- src/main/java/bgu/spl/mics/application/services/LeiaMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/LeiaMicroservice.java
@@ -1,14 +1,10 @@
 package bgu.spl.mics.application.services;
-import bgu.spl.mics.Callback;
-import bgu.spl.mics.Future;
-import bgu.spl.mics.MessageBusImpl;
-import bgu.spl.mics.MicroService;
+import bgu.spl.mics.*;
 import bgu.spl.mics.application.messages.AttackEvent;
 import bgu.spl.mics.application.messages.DeactivationEvent;
 import bgu.spl.mics.application.messages.TerminateBroadcast;
 import bgu.spl.mics.application.passiveObjects.Attack;
 import bgu.spl.mics.application.passiveObjects.Diary;
-import bgu.spl.mics.application.passiveObjects.Ewoks;
 
 /**
  * LeiaMicroservices Initialized with Attack objects, and sends them as  {@link AttackEvent}.
@@ -55,13 +51,10 @@ public class LeiaMicroservice extends MicroService {
         // -- send DeactivationEvent to R2D2 --
         messageBus.sendEvent(new DeactivationEvent());
         // -- subscribe to TerminateBroadcast and terminate accordingly --
-        this.subscribeBroadcast(TerminateBroadcast.class, new Callback<TerminateBroadcast>() {
-            @Override
-            public void call(TerminateBroadcast c) {
-                // TODO : call this object .terminate()
-            }
+        this.subscribeBroadcast(TerminateBroadcast.class, c -> {
+            Diary.getInstance().setLeiaTerminate(System.currentTimeMillis());
+            this.terminate();
         });
-        this.terminate();
         //------------------------------------------------------------------
     }
 }
