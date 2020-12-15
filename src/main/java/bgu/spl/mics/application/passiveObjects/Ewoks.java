54d4038b09c208998379610aac1f4386e8b6c800
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2020-12-15 22:40:58 +0200

    Omri -Fixed ewoks!!!!!

--- src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
@@ -3,7 +3,6 @@ import bgu.spl.mics.application.messages.AttackEvent;
 import java.util.*;
 
 
-
 /**
  * Passive object representing the resource manager.
  * <p>
@@ -13,84 +12,80 @@ import java.util.*;
  * You can add ONLY private methods and fields to this class.
  */
 public class Ewoks {
-    private static Ewoks instance = null;
-    private final ArrayList<Ewok> ewoksList;
-    private Boolean lock;
+    private static Ewoks instance = null; // singleton pattern
+    private ArrayList<Ewok> ewoksList;
 
+    private Ewoks() {
+        this.ewoksList = new ArrayList<>(0);
+    }
 
-    private Ewoks(int size, Boolean lock) { // constructor according to the size
+    private Ewoks(int size) {
         this.ewoksList = new ArrayList<>();
         for (int i=0; i < size; i++) {
             this.ewoksList.add(new Ewok(i));
         }
-        this.lock = lock;
     }
 
-
-    public static Ewoks getInstance(int size){ //Singleton pattern
-        if(instance == null){
-            //only on creation of first instance synchronize:
+    public static Ewoks getInstance() { // singleton pattern
+        if(instance == null) {
+            // only on creation of first instance synchronize:
             // this is to make sure only one thread creates the first instance
-            instance = new Ewoks(size, true);
+            synchronized (Ewoks.class) {
+                if(instance == null)
+                    instance = new Ewoks();
+            }
         }
         return instance;
     }
 
-
-    public static synchronized Ewoks getInstance() { // singleton pattern
+    public static Ewoks getInstance(int size) { // singleton pattern
+        if(instance == null) {
+            // only on creation of first instance synchronize:
+            // this is to make sure only one thread creates the first instance
+            synchronized (Ewoks.class) {
+                if(instance == null)
+                    instance = new Ewoks(size);
+            } // synchronized
+        } // if
         return instance;
     }
 
-
-
-
-
-    public boolean isAvailable(int serial) {
-        return (serial >= ewoksList.size()-1 || !ewoksList.get(serial-1).isAvailable());
-    }
-
-    public synchronized void acquire(int serialNumber) {
-        if(serialNumber < ewoksList.size())
-            ewoksList.get(serialNumber-1).acquire();
-    }
-
-    public void release(int serialNumber) { // it doesn't need to be synchronized (the only way to get here is thru a synchronized method)
-        if(serialNumber < ewoksList.size()) {
-            ewoksList.get(serialNumber-1).release();
-        }
-    }
-
-    public void acquireEwoks(List <Integer> serialNumbers) { // it doesn't need to be synchronized (the acquire of an ewok is synchronized)
-        Collections.sort(serialNumbers);
-        for (Integer serial : serialNumbers) {
-            while (!isAvailable(serial)) {
-                synchronized (lock) {
-                    try {
-                        this.wait(); // blocking!!
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+    public void acquire(int serialNumber) {
+        Ewok ewok = ewoksList.get(serialNumber-1);
+        synchronized (this) {
+            while (!ewok.available) {
+                try {
+                    wait(); // blocking!!
+                } catch (InterruptedException e) {
+                    System.out.println("InterruptedException on Ewoks acquire()");
+                    e.printStackTrace();
                 }
             } // while
-            acquire(serial);
-        } // for
+            ewok.acquire();
+        } // synchronized
     }
 
-    public synchronized void releaseEwoks(List<Integer> serialNumbers) {
-        for (Integer serial : serialNumbers) {
-            release(serial);
+    public void release(int serialNumber) {
+        Ewok ewok = ewoksList.get(serialNumber-1);
+        ewok.release();
+        synchronized (this) {
+            notifyAll(); // give waiting thread opportunity to catch the released Ewok
         }
-        notifyAll(); // give waiting thread opportunity to catch the released Ewok
     }
 
-    public static void initHanSoloAndC3P0(AttackEvent c, Ewoks ewoks) { // to spare code duplications
-        List<Integer> serialNumbers = c.getAttack().getSerials();
-        ewoks.acquireEwoks(serialNumbers);
+    public void ResourceManager(AttackEvent c) {
+        List<Integer> serials = c.getAttack().getSerials();
+        Collections.sort(serials); // prevent deadlock
+        for(int serial: serials) { // acquire all resources
+            this.acquire(serial); // blocking if ewok not available
+        }
         try { // all resources were acquired
             Thread.sleep(c.getAttack().getDuration());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        ewoks.releaseEwoks(serialNumbers);
+        for(int serial: serials) { // release all resources
+            this.release(serial);
+        }
     }
 }
